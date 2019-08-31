24588a9ef70c6d539330979896f08035efcd5073
middaysan <midda7@mail.ru>
2019-09-01 01:17:14 +0300

    add apply_status column to applies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_094931) do
+ActiveRecord::Schema.define(version: 2019_08_31_221601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_08_28_094931) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "status_apply", default: false
+    t.boolean "apply_status", default: false
     t.index ["project_id", "user_id"], name: "index_applies_on_project_id_and_user_id", unique: true
   end
 
